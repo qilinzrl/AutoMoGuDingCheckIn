47153bb0cf5f31967d163f29d72a85ebddf98f5f
qilinzrl <155358345+qilinzrl@users.noreply.github.com>
2025-10-28 18:13:31 +0800

    Update scheduled_runner.py

--- scheduled_runner.py
+++ scheduled_runner.py
@@ -20,8 +20,8 @@ logger = logging.getLogger("scheduler")
 # 设置调度器的日志标签
 _log_ctx.tag = "SCHEDULER"
 
-BASE_TIMES = ["18:00", "22:30"]  # 触发打卡的基础时间点
-MAX_OFFSET_MINUTES = 3  # 去机器化，随机让时间点偏移 0~10 分钟
+BASE_TIMES = ["18:16", "22:30"]  # 触发打卡的基础时间点
+MAX_OFFSET_MINUTES = 0  # 去机器化，随机让时间点偏移 0~10 分钟
 
 
 def generate_daily_schedule(day: date) -> List[datetime]:
